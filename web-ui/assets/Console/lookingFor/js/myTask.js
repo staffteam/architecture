ce19542264ab7b805ef0f2e2fec267529c8ec48b
LonelyHobo <1144646550@qq.com>
2020-03-26 23:11:48 +0800

    我的任务栏添加上传功能

--- web-ui/assets/Console/lookingFor/js/myTask.js
+++ web-ui/assets/Console/lookingFor/js/myTask.js
@@ -1,3 +1,4 @@
+let file_name = "";
 $(function () {
     var element = layui.element;
     table.render({
@@ -9,7 +10,9 @@ $(function () {
             ,
         cols: [
             [ //表头
-                {type:'checkbox'},
+                {
+                    type: 'checkbox'
+                },
                 {
                     field: 'id',
                     title: 'ID'
@@ -52,19 +55,62 @@ $(function () {
                 }
             ]
         ],
-        done:function(){
-            $('a.notup').each(function(){
+        done: function () {
+            $('a.notup').each(function () {
                 $(this).closest('tr').addClass('not-up-tr');
             })
+
+            $('#downWord').click(function () {
+                $('#down span').click();
+            })
+            var upload = layui.upload;
+            //普通图片上传
+            var uploadInst = upload.render({
+                elem: '#upWord',
+                url: 'http://localhost:8083/api/image',
+                progress: function (n, elem) {
+                    upWin.percent(file_name, n);
+                    console.log(elem); //得到当前触发的元素 DOM 对象。可通过该元素定义的属性值匹配到对应的进度条。
+                },
+                before: function (obj) {
+                    //预读本地文件示例，不支持ie8
+                    obj.preview(function (index, file, result) {
+                        upWin.init(function () {
+                            file_name = file.name;
+                            upWin.start(file.name);
+                        });
+                    });
+                },
+                done: function (res) {
+                    //如果上传失败
+                    if (res.code > 0) {
+                        return $msg({
+                            content: '上传失败',
+                            icon: 0
+                        });
+                    }
+                    return $msg({
+                        content: '成功',
+                        icon: 1
+                    });
+                    //上传成功
+                },
+                error: function () {
+                    $msg({
+                        content: '上传失败',
+                        icon: 0
+                    });
+                }
+            });
         }
     });
     // 选择类别
-    $('#myTaskform .l button').click(function(){
-        if($(this)[0].getAttribute('data-id')==0){
+    $('#myTaskform .l button').click(function () {
+        if ($(this)[0].getAttribute('data-id') == 0) {
             //初步设计阶段
-        }else{
+        } else {
             //施工图阶段
         }
-         $(this).removeClass('layui-btn-white').siblings().addClass('layui-btn-white');
+        $(this).removeClass('layui-btn-white').siblings().addClass('layui-btn-white');
     })
 })
